50bc7bb47ebca592bc4865537da69db2027d3cf5
venkatpathuri <venkatpathuri123@gmail.com>
2025-07-22 08:28:29 +0530

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
